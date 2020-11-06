2f040bfdae76a556b6e02d55c07f2c8a3a896e45
devebrahim <sadeghiebrahim.dev@gmail.com>
2020-11-06 21:37:54 +0330

    add angular and scss

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,32 @@ import { timer, Subscription } from 'rxjs';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'pomodoro';
-}
+export class AppComponent implements OnInit {
+  title = 'pomodoro-project';
+  II:number=  5
+  timeLeft: number = 60;
+  minit: number = 2;
+  interval;
+  interval2;
+  constructor() {}
+  startTimer() {
+    this.interval = setInterval(() => {
+       if(this.timeLeft > 55) {
+        this.timeLeft--;} 
+      else{
+        this.timeLeft=60;
+        this.minit--;}
+      },1000)  
+    this.interval2 = setInterval(() => {
+      if(this.timeLeft==56 && this.minit==0) {
+       alert("ghe");
+       clearInterval(this.interval)
+       clearInterval(this.interval2)
+     }
+      else;},1000)}
+  pauseTimer() {
+    clearInterval(this.interval);
+    clearInterval(this.interval2)
+  }
+  ngOnInit() {}
+}  
